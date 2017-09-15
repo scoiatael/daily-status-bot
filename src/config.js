e543a02b53349f2fe63430dd12244fadd9374c58
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2017-09-15 13:19:29 +0200

    Ditch dotenv in favor of direnv

--- src/config.js
+++ src/config.js
@@ -1,10 +1,7 @@
 
 'use strict'
 
-const dotenv = require('dotenv')
 const ENV = process.env.NODE_ENV || 'development'
-
-if (ENV === 'development') dotenv.load()
 
 const config = {
   ENV: process.env.NODE_ENV,
